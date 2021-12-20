1b1ff67e3cfb0f975087b8e6f7c6276058d8c9b6
EdenGenetTasama <edentasama19@gmail.com>
2021-12-20 13:50:50 +0200

    tesk

--- LinQConsoleApp/Program.cs
+++ LinQConsoleApp/Program.cs
@@ -43,21 +43,69 @@
 
 // 4 אובייקטים : 
 
-Person[] arrayOfPersons = new Person[] { new Person("Edenio" , "Tasama" , 1996 ),
-new Person("Ross", "Geler", 2010),
-new Person("Daniel", "Tal", 2018),
-new Person("Monica", "Geler", 1996),
+//Person[] arrayOfPersons = new Person[] { new Person("Edenio" , "Tasama" , 1996 ),
+//new Person("Ross", "Geler", 2010),
+//new Person("Daniel", "Tal", 2018),
+//new Person("Monica", "Geler", 1996),
+//};
+
+//List<Person> listOfPerson = arrayOfPersons.Where(item => item.name.Length > 4).OrderBy(item => item.yearOfBirth).ToList();
+//Person objThatRepeat = listOfPerson.Find(item => item.yearOfBirth == 1996);
+//Console.WriteLine(objThatRepeat.name);
+
+//foreach (Person person in listOfPerson)
+//{
+//    Console.WriteLine($"{person.name} , {person.yearOfBirth}");
+//}
+
+//1 : 
+
+string[] namesArray = new string[5] { "Eden", "Daniel", "Ross", "Febi", "avi" };
+
+List <string> listOfNames = namesArray.Where(item => item.ToUpper()[0] == 'A').ToList();
+
+//foreach(string name in listOfNames)
+//{
+//    Console.WriteLine(name);
+//}
+
+//2 : 
+
+int [] arrayOfAges = new int[10] {20,14,25,69,87,12,10,8,40,80};
+
+List<int> listOfAges = arrayOfAges.Where(item=> item> 18).ToList();
+
+//foreach (int age in listOfAges)
+//{
+//    Console.WriteLine(age);
+//}
+
+//3:
+
+List <User> listOfUsers = new List<User> {
+  new User("Edenio" , "Tasama" , 25 ),
+new User("Ros", "Geler", 50),
+new User("Daniel", "Tal", 90),
+new User("Monica", "Geler", 10),
 };
 
-List<Person> listOfPerson = arrayOfPersons.Where(item => item.name.Length > 4).OrderBy(item => item.yearOfBirth).ToList();
-Person objThatRepeat = listOfPerson.Find(item => item.yearOfBirth == 1996);
-Console.WriteLine(objThatRepeat.name);
+//List<User> afterLinqList = listOfUsers.Where(item => item.age > 21 ).ToList();
 
-foreach (Person person in listOfPerson)
-{
-    Console.WriteLine($"{person.name} , {person.yearOfBirth}");
-}
+//foreach(User age in afterLinqList)
+//{
+//    Console.WriteLine(age.age);
 
+//}
 
 
+//4:
+List<User> afterLinqListOver4Lenger = listOfUsers.Where(item=> item.name.Length > 4).ToList();
+
+//foreach(User user in afterLinqListOver4Lenger)
+//{
+//    Console.WriteLine(user.name);
+//}
+
+
+//5
 
---
+++ LinQConsoleApp/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQConsoleApp
+{
+    internal class User
+    {
+        public string name;
+        public string lName;
+        public int age;
+
+        public User(string _name, string _Lname, int _age)
+        {
+            this.name = _name;
+            this.lName = _Lname;
+            this.age = _age;
+        }
+    }
+}
